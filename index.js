4457d9ff1656434e08b00d5ea3b7912cefbe7941
Marcio Galli <taboca@gmail.com>
2021-04-09 17:02:58 -0300

    Improving results and date time

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Binance = require('node-binance-api');
-const Keys    = require('./config.js');
+const Keys    = require('./config-REMOVETHIS.js');
 
 const binance = new Binance().options(Keys);
 
@@ -9,7 +9,20 @@ let options = {
   limit: 2 // Limit results to two entries
 };
 
-binance.trades("BTCBRL", (error, trades, symbol) => {
-  console.info(symbol+" trade history", trades);
+binance.trades("BTCBRL", (error, trades, symbol) => {  
+  for(let k in trades) { 
+    let trade = trades[k];
+    let tradeDateTime = new Date(trade.time);
+    console.log(`Trade event --------------------------------`);
+    console.log(`time             : ${tradeDateTime}`);
+    console.log(`symbol           : ${trade.symbol}`);
+    console.log(`price            : ${trade.price}`);
+    console.log(`quantity         : ${trade.qty}`);
+    console.log(`commission fee   : ${trade.commission}`);
+    console.log(`commission asset : ${trade.commissionAsset}`);
+    console.log(`Is buyer         : ${trade.isBuyer}`);
+    console.log(`Is Maker         : ${trade.isMaker}`);
+//    console.log(trade);
+  }
 }, options);
 
